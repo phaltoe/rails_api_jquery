493af9f9bdd350ce604cca6cfa145f67ea323542
phaltoe <reverse9@live.com>
2016-10-04 09:22:03 -0300

    created first course models and some seed data to play

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Course.create(name: "Ruby", details: "Learn to Fly")
+Course.create(name: "React", details: "I am Flying")
+Course.create(name: "JS", details: "Your Wings")
